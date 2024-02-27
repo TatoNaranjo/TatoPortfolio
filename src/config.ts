5fdcd48a7729aae63b6fc6bfd901fac9b4fc5fc3
SANTIAGO NARANJO HERRERA <snaranjoh@ucundinamarca.edu.co>
2024-02-27 11:55:42 -0500

    Add Initial Template for the project

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'TatoNaranjo';
+export const SITE_DESCRIPTION = 'Portafolio Personal de Santiago Naranjo Herrera';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
